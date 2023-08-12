98ad45dda5624f942f8690cf0985e3b44b0b4e10
Steve Waldman <swaldman@mchange.com>
2023-08-12 18:19:28 -0400

    Authentication via validation of a JWT token works!

--- hotsauce/HotSauceServer.scala
+++ hotsauce/HotSauceServer.scala
@@ -1,12 +1,22 @@
 package hotsauce
 
-import sttp.model.{StatusCode,QueryParams}
+import java.util.Base64
+import scala.io.Source
+import scala.util.Using
+import sttp.model.{QueryParams, StatusCode}
 import sttp.tapir.ztapir.*
 import sttp.tapir.json.jsoniter.*
-import sttp.tapir.{EndpointOutput,Schema}
+import sttp.tapir.{EndpointOutput, Schema}
 import sttp.tapir.server.ziohttp.ZioHttpInterpreter
 import zio.*
 import zio.http.Server
+
+// import pdi.jwt.{Jwt, JwtAlgorithm}
+import io.jsonwebtoken.Jwts
+import java.security.Key
+import java.security.cert.CertificateFactory
+
+
 
 object HotSauceServer extends ZIOAppDefault:
 
@@ -25,14 +35,16 @@ object HotSauceServer extends ZIOAppDefault:
   )
 
   object TapirEndpoint:
-    val Base        = endpoint.in("api").in("hotsauces").errorOut(either404or500)
+    val Base          = endpoint.in("api").in("hotsauces").errorOut(either404or500)
+    val Authenticated = Base.securityIn( auth.bearer[String]() )
+
     val GetAll      = Base.get.in(queryParams).out(jsonBody[List[HotSauce]])
     val GetCount    = Base.get.in("count").out(jsonBody[Long])
     val GetById     = Base.get.in(path[Long]).out(jsonBody[HotSauce])
-    val PostNoId    = Base.post.in(jsonBody[HotSauceData]).out(jsonBody[HotSauce])
-    val PostWithId  = Base.post.in(path[Long]).in(jsonBody[HotSauceData]).out(jsonBody[HotSauce])
-    val PutById     = Base.put.in(path[Long]).in(jsonBody[HotSauceData]).out(jsonBody[HotSauce])
-    val DeleteById  = Base.delete.in(path[Long]).out(jsonBody[HotSauce])
+    val PostNoId    = Authenticated.post.in(jsonBody[HotSauceData]).out(jsonBody[HotSauce])
+    val PostWithId  = Authenticated.post.in(path[Long]).in(jsonBody[HotSauceData]).out(jsonBody[HotSauce])
+    val PutById     = Authenticated.put.in(path[Long]).in(jsonBody[HotSauceData]).out(jsonBody[HotSauce])
+    val DeleteById  = Authenticated.delete.in(path[Long]).out(jsonBody[HotSauce])
   end TapirEndpoint
 
   def allFiltered( db : HotSauceDb )( params : QueryParams ) : ZOut[List[HotSauce]] =
@@ -64,40 +76,83 @@ object HotSauceServer extends ZIOAppDefault:
     requireField( data.url,         "url"         )
     requireField( data.heat,        "heat"        )
 
-  def createNew( db : HotSauceDb )( data : HotSauceData ) : ZOut[HotSauce] =
+  // this is a placeholder.
+  //
+  // a real application might put information extracted from the JWT into
+  // an object like this to make more fine-grained authentication decisions.
+  case class AuthenticationInfo()
+
+  def authenticate( key : Key )( bearerToken : String ) : ZOut[AuthenticationInfo] =
+    val task = ZIO.attempt:
+      //val decoded = Jwt.decode(bearerToken, secret, Seq(JwtAlgorithm.RS256)).get
+      val decoded =
+        Jwts.parserBuilder()
+          .setSigningKey(key)
+          .build()
+          .parse(bearerToken)
+      println(s"Decoded JWT: ${decoded}")
+      AuthenticationInfo() // someday, maybe I'll look into the decoded key and include real information
+    mapPlainError(task)
+
+  def createNew( db : HotSauceDb )( auth : AuthenticationInfo )( data : HotSauceData ) : ZOut[HotSauce] =
     val task = ZIO.attemptBlocking:
       requireAllDataFields( data )
       db.save( data.brandName.get, data.sauceName.get, data.description.get, data.url.get, data.heat.get )
     mapPlainError( task )
     
-  def createNewWithId( db : HotSauceDb )( id : Long, data : HotSauceData ) : ZOut[HotSauce] =
+  def createNewWithId( db : HotSauceDb )( auth : AuthenticationInfo )( id : Long, data : HotSauceData ) : ZOut[HotSauce] =
     val task = ZIO.attemptBlocking:
       requireAllDataFields(data)
       db.save( id, data.brandName.get, data.sauceName.get, data.description.get, data.url.get, data.heat.get )
     mapPlainError( task )
 
-  def updateById( db : HotSauceDb )( id : Long, data : HotSauceData ) : ZOut[HotSauce] =
+  def updateById( db : HotSauceDb )( auth : AuthenticationInfo )( id : Long, data : HotSauceData ) : ZOut[HotSauce] =
     mapPlainError( ZIO.attemptBlocking( db.update( id, data.brandName, data.sauceName, data.description, data.url, data.heat ) ) )
 
-  def deleteById( db : HotSauceDb )( id : Long ) : ZOut[HotSauce] =
+  def deleteById( db : HotSauceDb )( auth : AuthenticationInfo )( id : Long ) : ZOut[HotSauce] =
     mapMaybeError( ZIO.attemptBlocking( db.deleteById(id) ) )
 
-  def serverEndpoints( db : HotSauceDb ) =
+  def serverEndpoints( key : Key, db : HotSauceDb ) : List[ZServerEndpoint[Any,Any]] =
     import TapirEndpoint.*
     List (
-      GetAll.zServerLogic( allFiltered(db) ),
-      GetCount.zServerLogic( count(db) ),
-      GetById.zServerLogic( oneById(db) ),
-      PostNoId.zServerLogic( createNew(db) ),
-      PostWithId.zServerLogic( createNewWithId(db) ),
-      PutById.zServerLogic( updateById(db) ),
-      DeleteById.zServerLogic( deleteById(db) ),
-    ).map( _.widen[Any] ) // annoying tapir / Scala 3 type inference issue
+      // simple endpoints
+      GetAll.zServerLogic( allFiltered(db) ).widen[Any], // annoying tapir / Scala 3 type inference issue
+      GetCount.zServerLogic( count(db) ).widen[Any],
+      GetById.zServerLogic( oneById(db) ).widen[Any],
+
+      // authenticated endpoints
+      PostNoId.zServerSecurityLogic( authenticate(key) ).serverLogic( createNew(db) ),
+      PostWithId.zServerSecurityLogic( authenticate(key) ).serverLogic( createNewWithId(db) ),
+      PutById.zServerSecurityLogic( authenticate(key) ).serverLogic( updateById(db) ),
+      DeleteById.zServerSecurityLogic( authenticate(key) ).serverLogic( deleteById(db) ),
+    )
+
+  def keyFromCertificatePemUrl( pemUrl : String ) : Key =
+    // see https://stackoverflow.com/questions/6358555/obtaining-public-key-from-certificate
+    Using.resource( new java.io.BufferedInputStream( new java.net.URL(pemUrl).openStream ) ): is =>
+      val cf = CertificateFactory.getInstance("X.509");
+      val certificate = cf.generateCertificate(is)
+      certificate.getPublicKey()
+
+    /*
+    // this would be from a PUBLIC KEY pem
+    // see https://stackoverflow.com/questions/24223275/when-to-use-x509encodedkeyspec-vs-rsapublickeyspec
+    Using.resource(Source.fromURL(pemUrl)): source =>
+      val goodStuff = source.getLines().filterNot( _.startsWith("-----")).mkString
+      val bytes = Base64.getDecoder().decode(goodStuff)
+      val keySpec = new X509EncodedKeySpec(bytes)
+      val kf = KeyFactory.getInstance("RSA"); // We expect an "RS256" (RSA) public key
+      kf.generatePublic(keySpec)
+    */
+
 
   override def run =
     for
+      args       <- getArgs
+      pemUrl     <- if args.size > 0 then ZIO.succeed(args(0)) else ZIO.fail("No URL provided to pem file of public key by which to decode the JWT token.")
+      key        = keyFromCertificatePemUrl(pemUrl)
       db         <- ZIO.succeed( DemoHotSauceDb )
-      sEndpoints = serverEndpoints(db)
+      sEndpoints = serverEndpoints(key, db)
       httpApp    = ZioHttpInterpreter().toHttp(sEndpoints)
       exitCode   <- Server.serve(httpApp.withDefaultErrorResponse).provide(ZLayer.succeed(Server.Config.default.port(8080)), Server.live).exitCode
     yield exitCode                           
--- scala-cli-config.scala
+++ scala-cli-config.scala
@@ -5,9 +5,13 @@
 //> using dep "com.softwaremill.sttp.tapir::tapir-jsoniter-scala:1.7.0"
 //> using dep "com.github.plokhotnyuk.jsoniter-scala::jsoniter-scala-core:2.23.2"
 //> using dep "com.github.plokhotnyuk.jsoniter-scala::jsoniter-scala-macros:2.23.2"
+//> using dep "io.jsonwebtoken:jjwt-api:0.11.5"
+//> using dep "io.jsonwebtoken:jjwt-impl:0.11.5"
+//> using dep "io.jsonwebtoken:jjwt-jackson:0.11.5"
 
 // //> using dep "com.softwaremill.sttp.tapir::tapir-json-upickle:1.7.0"
 // //> using dep "com.lihaoyi::cask:0.9.1"
 // //> using dep "com.lihaoyi::upickle:3.1.2"
+// //> using dep "com.github.jwt-scala::jwt-core:9.4.3"
 
 
